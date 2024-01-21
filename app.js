5e85c654ca905979f8ea9975d5540025898447c3
SR-Rony <srrony707@gmail.com>
2024-01-21 22:27:18 +0600

    html file add and use body-parser

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let express =require("express")
+let bodyParser = require('body-parser')
 let app = express()
 let usersRouting = require('./routes/users.route')
 
 app.use("/api/users",usersRouting)
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post("/",(req,res)=>{
+    let body = req.body.name
+    res.send(`this is ${body}`)
+})
 app.get("/",(req,res)=>{
-    res.send("<h1>iam get request and home router</h1>")
+    res.statusCode = 200
+    res.sendFile(__dirname + "/pages/home.html")
+    // res.end()
+})
+app.get("/login",(req,res)=>{
+    res.statusCode = 200
+    res.sendFile(__dirname + "/pages/login.html")
+    // res.end()
 })
 
 app.get("*",(req,res)=>{
