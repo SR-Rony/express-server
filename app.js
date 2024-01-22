87cc4d7bdc664221bdb1639875351b7a0d69f6aa
SR-Rony <srrony707@gmail.com>
2024-01-22 10:12:32 +0600

    add dotenv and meddleware add

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ let bodyParser = require('body-parser')
 let app = express()
 let usersRouting = require('./routes/users.route')
 
-app.use("/api/users",usersRouting)
+app.use("/users",usersRouting)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post("/",(req,res)=>{
-    let body = req.body.name
-    res.send(`this is ${body}`)
-})
-app.get("/",(req,res)=>{
-    res.statusCode = 200
+const meddle = (req,res,next)=>{
+    console.log('i am meddle wear function');
+    req.time = Date(Date.now())
+    next()
+}
+
+
+app.get("/",meddle,(req,res)=>{
+    console.log('current time '+ req.time);
     res.sendFile(__dirname + "/pages/home.html")
     // res.end()
 })
+app.get("/register",(req,res)=>{
+    res.statusCode = 200
+    res.sendFile(__dirname + "/pages/register.html")
+    // res.end()
+})
+
+app.post("/register",(req,res)=>{
+    let fullName = req.body.fullName
+    let email = req.body.email
+
+    res.send(`<h1>user Fullname is : ${fullName} ane user email : ${email}</h1>`)
+    // res.end()
+})
+
+
+
 app.get("/login",(req,res)=>{
     res.statusCode = 200
     res.sendFile(__dirname + "/pages/login.html")
+    // res.end()
+})
+
+app.get("/product/:id([0-3]+)",(req,res)=>{
+    res.statusCode = 200
+    res.send(`<h1>ID : ${req.params.id}</h1>`)
     // res.end()
 })
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require("dotenv").config()
 let app = require("./app")
-let PORT = 300
+let PORT = process.env.PORT || 300
 
 app.listen(PORT,()=>{
     console.log(`my server is runing at http://localhost:${PORT}`);
